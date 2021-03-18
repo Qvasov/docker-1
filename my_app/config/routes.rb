27d86edccc93bf0bda72f906f69e31a2c9df9d1d
dbennie <dbennie@student.21-school.ru>
2021-03-19 00:19:00 +0300

    docker

--- my_app/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'tasks#index'
-  resources :tasks
-  patch '/done/:id', to: 'tasks#done', as: :done_task
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
